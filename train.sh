94ede00bb516ef550638918bbdd1624578f6bcc1
yoshonabee <jhann0120@gmail.com>
2019-04-04 16:48:39 +0800

    code final

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
-./train 300 model_init.txt data/train/seq_model_01.txt model/model_01.txt
-./train 300 model_init.txt data/train/seq_model_02.txt model/model_02.txt
-./train 300 model_init.txt data/train/seq_model_03.txt model/model_03.txt
-./train 300 model_init.txt data/train/seq_model_04.txt model/model_04.txt
-./train 300 model_init.txt data/train/seq_model_05.txt model/model_05.txt
+./train 150 model_init.txt data/train/seq_model_01.txt model/model_01.txt
+./train 150 model_init.txt data/train/seq_model_02.txt model/model_02.txt
+./train 150 model_init.txt data/train/seq_model_03.txt model/model_03.txt
+./train 150 model_init.txt data/train/seq_model_04.txt model/model_04.txt
+./train 150 model_init.txt data/train/seq_model_05.txt model/model_05.txt
